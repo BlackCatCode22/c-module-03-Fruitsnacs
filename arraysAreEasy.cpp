389aa72fe1971cddfb84e51be2a7039e9d900f34
Fruitsnacs <61921415+Fruitsnacs@users.noreply.github.com>
2025-02-13 18:21:44 -0800

    Add files via upload

---
+++ arraysAreEasy.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = { 1, 2, 3, 4, 5 };
+    int* p = arr;
+    for (int i = 0; i < 5; ++i) {
+        cout << "Address of arr[" << i << "]: " << (p + i) << endl;
+        cout << "Value of arr[" << i << "]: " << *(p + i) << endl;
+    }
+    return 0;
+}
